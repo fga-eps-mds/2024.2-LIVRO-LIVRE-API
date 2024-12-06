b2d7f3d8a30ded9e50043b594dc83a62b0cfc72c
Joaovitor045 <joaovitoralvestf@gmail.com>
2024-12-06 08:58:10 -0300

    feat:create a method put to update users

---
+++ src/users/dtos/updateUser.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  firstName: string;
+  
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateUserDto } from './dtos/updateUser.dto'; 
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../database/entities/user.entity';
@@ -16,6 +17,13 @@ export class UsersService {
 
   findOne(id: string): Promise<User | null> {
     return this.usersRepository.findOneBy({ id });
+  }
+
+  async update(id: string, updateData: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException();
+    await this.usersRepository.update(id, updateData);
+    return this.findOne(id);
   }
 
   async remove(id: string): Promise<void> {
